354ae46e622a87573ea99924147307b06f6960b9
Stray228 <56656430+Stray228@users.noreply.github.com>
2023-06-15 20:55:56 +0300

    React-Router, add skeleton
    Route to Home, Cart. Add skeleton.
    Split app into components

---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const Skeleton = () => (
+  <ContentLoader
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={465}
+    viewBox="0 0 280 465"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb">
+    <circle cx="134" cy="136" r="125" />
+    <rect x="0" y="269" rx="10" ry="10" width="280" height="23" />
+    <rect x="0" y="308" rx="10" ry="10" width="280" height="88" />
+    <rect x="126" y="410" rx="20" ry="20" width="152" height="45" />
+    <rect x="0" y="418" rx="10" ry="10" width="95" height="30" />
+  </ContentLoader>
+);
+
+export default Skeleton;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+
+export const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://6489e0555fa58521cab0682f.mockapi.io/items')
+      .then((res) => res.json())
+      .then((json) => {
+        setItems(json);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {loading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+};

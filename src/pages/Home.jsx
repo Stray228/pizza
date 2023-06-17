c5e45a383a7e534e7364ef4acdbb7018e99d600b
Stray228 <56656430+Stray228@users.noreply.github.com>
2023-06-17 17:04:40 +0300

    Add fetch sort and popup back-end
    State category sort in home, add fetch mockapi back-end(sort and popup- plug)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,22 +8,35 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 export const Home = () => {
   const [items, setItems] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  });
 
   React.useEffect(() => {
-    fetch('https://6489e0555fa58521cab0682f.mockapi.io/items')
+    setLoading(true);
+
+    const sortBy = sortType.sortProperty.replace('-', '');
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+
+    fetch(
+      `https://6489e0555fa58521cab0682f.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+    )
       .then((res) => res.json())
       .then((json) => {
         setItems(json);
         setLoading(false);
       });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">

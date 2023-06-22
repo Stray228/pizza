4b90f01b5a8af124b7e6099b59fbde44b6c69a25
Stray228 <56656430+Stray228@users.noreply.github.com>
2023-06-22 20:30:26 +0300

    redux cart
    redux cart

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -31,9 +31,7 @@ function Sort() {
     };
     document.body.addEventListener('click', handleClickOutside);
 
-    return () => {
-      document.body.removeEventListener('click', handleClickOutside);
-    };
+    return () => document.body.removeEventListener('click', handleClickOutside);
   }, []);
 
   return (

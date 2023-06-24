e81306f04d8c864285d2275ba7908e445ac7cc20
Stray228 <56656430+Stray228@users.noreply.github.com>
2023-06-24 17:33:35 +0300

    useParams, useLocation, Outlet
    useParams, useLocation, Outlet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import qs from 'qs';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import {
   selectFilter,
@@ -93,7 +93,11 @@ export const Home = () => {
     isMounted.current = true;
   }, [currentPage, categoryId, sort.sortProperty, searchValue]);
 
-  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const pizzas = items.map((obj) => (
+    <Link key={obj.id} to={`/pizza/${obj.id}`}>
+      <PizzaBlock {...obj} />
+    </Link>
+  ));
   const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
   return (
